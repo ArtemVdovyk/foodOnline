d9a017149257f9799b3eb292d7669738b28a6d43
ArtemVdovyk <artem.vdovyk@gmail.com>
2024-04-09 09:34:47 +0300

    Added UserProfile model

--- accounts/models.py
+++ accounts/models.py
@@ -68,7 +68,7 @@ class User(AbstractBaseUser):
 
     objects = UserManager()
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.email
 
     def has_perm(self, perm, obj=None):
@@ -76,3 +76,25 @@ class User(AbstractBaseUser):
 
     def has_module_perms(self, app_label):
         return True
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                blank=True, null=True)
+    profile_picture = models.ImageField(upload_to="users/profile_pictures",
+                                        blank=True, null=True)
+    cover_photo = models.ImageField(upload_to="users/cover_photos",
+                                    blank=True, null=True)
+    address_line_1 = models.CharField(max_length=50, blank=True, null=True)
+    address_line_2 = models.CharField(max_length=50, blank=True, null=True)
+    country = models.CharField(max_length=15, blank=True, null=True)
+    state = models.CharField(max_length=15, blank=True, null=True)
+    city = models.CharField(max_length=15, blank=True, null=True)
+    pin_code = models.CharField(max_length=6, blank=True, null=True)
+    latitude = models.CharField(max_length=20, blank=True, null=True)
+    longitude = models.CharField(max_length=20, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.user.email
