b9911bef7c70bd439466b6fd781a986b4bec32a3
ArtemVdovyk <artem.vdovyk@gmail.com>
2024-04-26 16:48:57 +0300

    Prepared address field for Google Geocoding

--- accounts/forms.py
+++ accounts/forms.py
@@ -26,6 +26,10 @@ class UserForm(forms.ModelForm):
 
 
 class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(
+        widget=forms.TextInput(attrs={"placeholder": "Start typing...",
+                                      "required": "required"})
+    )
     profile_picture = forms.FileField(
         widget=forms.FileInput(attrs={"class": "btn btn-info"}),
         validators=[allow_only_images_validator],
@@ -44,8 +48,8 @@ class UserProfileForm(forms.ModelForm):
 
     class Meta:
         model = UserProfile
-        fields = ["profile_picture", "cover_photo", "address_line_1",
-                  "address_line_2", "country", "state", "city", "pin_code",
+        fields = ["profile_picture", "cover_photo", "address",
+                  "country", "state", "city", "pin_code",
                   "latitude", "longitude"]
 
     # Make fields read-only. Option 2
