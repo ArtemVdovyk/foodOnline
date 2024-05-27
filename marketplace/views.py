e12f447ad56bb47bea520286bf83f4fab999a12b
ArtemVdovyk <artem.vdovyk@gmail.com>
2024-05-27 14:48:31 +0300

    Added decreasr cart functionality

--- marketplace/views.py
+++ marketplace/views.py
@@ -71,3 +71,35 @@ def add_to_cart(request, food_id):
             return JsonResponse({"status": "Failed", "message": "Invalid request"})
     else:
         return JsonResponse({"status": "Failed", "message": "Please login to continue"})
+
+
+def decrease_cart(request, food_id):
+    if request.user.is_authenticated:
+        if request.headers.get("x-requested-with") == "XMLHttpRequest":
+            # Check if the food item exists
+            try:
+                food_item = FoodItem.objects.get(id=food_id)
+                # Check if the user has already added that food to the cart
+                try:
+                    check_cart = Cart.objects.get(user=request.user,
+                                                  food_item=food_item)
+                    if check_cart.quantity > 1:
+                        # Decrease cart quantity
+                        check_cart.quantity -= 1
+                        check_cart.save()
+                    else:
+                        check_cart.delete()
+                        check_cart.quantity = 0
+                    return JsonResponse({"status": "Success",
+                                         "cart_counter": get_cart_counter(request),
+                                         "qty": check_cart.quantity})
+                except:
+                    return JsonResponse({"status": "Failed",
+                                         "message": "You do not have this item in your cart!"})
+            except:
+                return JsonResponse({"status": "Failed",
+                                     "message": "This food does not exist!"})
+        else:
+            return JsonResponse({"status": "Failed", "message": "Invalid request"})
+    else:
+        return JsonResponse({"status": "Failed", "message": "Please login to continue"})
