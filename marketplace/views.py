1412f442846085142ee423c2fa8e03f345239604
ArtemVdovyk <artem.vdovyk@gmail.com>
2024-08-02 14:57:49 +0300

    Added is_open member function to display open-close badge

--- marketplace/views.py
+++ marketplace/views.py
@@ -35,20 +35,10 @@ def vendor_detail(request, vendor_slug):
 
     opening_hours = OpeningHour.objects.filter(
         vendor=vendor).order_by("day", "-from_hour")
+
     today = date.today().isoweekday()
     current_opening_hours = OpeningHour.objects.filter(
         vendor=vendor, day=today)
-    current_time = datetime.now().strftime("%H:%M:%S")
-    is_open = None
-    for i in current_opening_hours:
-        start = str(datetime.strptime(i.from_hour, "%I:%M %p").time())
-        end = str(datetime.strptime(i.to_hour, "%I:%M %p").time())
-        if current_time > start and current_time < end:
-            is_open = True
-            break
-        else:
-            is_open = False
-
 
     if request.user.is_authenticated:
         cart_items = Cart.objects.filter(user=request.user)
@@ -60,7 +50,6 @@ def vendor_detail(request, vendor_slug):
         "cart_items": cart_items,
         "opening_hours": opening_hours,
         "current_opening_hours": current_opening_hours,
-        "is_open": is_open,
     }
     return render(request, 'marketplace/vendor_detail.html', context=context)
 
