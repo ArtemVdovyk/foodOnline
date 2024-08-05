7221380110a3359407540cfd7ff52b352fc99736
ArtemVdovyk <artem.vdovyk@gmail.com>
2024-08-05 10:27:25 +0300

    Added updating customer profile

--- customers/views.py
+++ customers/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from accounts.forms import UserProfileForm, UserInfoForm
 from accounts.models import UserProfile
@@ -8,10 +9,25 @@ from accounts.models import UserProfile
 @login_required(login_url="login")
 def customer_profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
-    profile_form = UserProfileForm(instance=profile)
-    user_form = UserInfoForm(instance=request.user)
+    if request.method == "POST":
+        profile_form = UserProfileForm(
+            request.POST, request.FILES, instance=profile)
+        user_form = UserInfoForm(request.POST, instance=request.user)
+
+        if profile_form.is_valid() and user_form.is_valid():
+            profile_form.save()
+            user_form.save()
+            messages.success(request, "Profile updated")
+            return redirect("customer_profile")
+        else:
+            print(profile_form.errors)
+            print(user_form.errors)
+    else:
+        profile_form = UserProfileForm(instance=profile)
+        user_form = UserInfoForm(instance=request.user)
 
     context = {
+        "profile": profile,
         "profile_form": profile_form,
         "user_form": user_form,
     }
