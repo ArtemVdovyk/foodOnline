411471924bf0a8d5283d9a4d756a141b94d92505
ArtemVdovyk <artem.vdovyk@gmail.com>
2024-08-07 10:12:00 +0300

    Added calculating vendor order total data

--- orders/models.py
+++ orders/models.py
@@ -68,11 +68,33 @@ class Order(models.Model):
 
     def get_total_by_vendor(self):
         vendor = Vendor.objects.get(user=request_object.user)
+        subtotal = 0
+        tax = 0
+        tax_dict = {}
         if self.total_data:
             total_data = json.loads(self.total_data)
             data = total_data.get(str(vendor.id))
 
-        return vendor
+            for key, value in data.items():
+                subtotal += float(key)
+                value = value.replace("'", '"')
+                value = json.loads(value)
+                tax_dict.update(value)
+
+                # Calculate Tax
+                # {'CGST': {'9.00': '2.70'}, 'SGST': {'7.00': '2.10'}}
+                for i in value:
+                    for j in value[i]:
+                        tax += float(value[i][j])
+
+        grand_total = float(subtotal) + float(tax)
+        context = {
+            "subtotal": subtotal,
+            "tax": tax,
+            "tax_dict": tax_dict,
+            "grand_total": grand_total,
+        }
+        return context
 
     def __str__(self):
         return self.order_number
